a377fd8b026f7bbbff29cc7c88f21c14753c8eba
nauman-azeem <borissims4@gmail.com>
2018-07-20 02:39:55 +0500

    user edit & index view photo component modified

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -19,7 +19,7 @@
         @foreach($users as $user)
           <tr>
             <td>{{$user->id}}</td>
-            <td><img height="50px" src="{{$user->photo ? $user->photo->file: 'No UserPhoto'}}" alt="" /></td>
+            <td><img height="50px" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="" class="img-responsive img-rounded"></td>
             <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
             <td>{{$user->email}}</td>
             <td>{{$user->role->name}}</td>

c4649279d5f56dcc3b2f24dcbd088e4198f6af8e
nauman-azeem <borissims4@gmail.com>
2018-07-20 02:07:57 +0500

    create user with photo feature completed

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -25,9 +26,14 @@ class AdminUsersController extends Controller
   public function store(UsersRequest $request)
   {
     $input = $request->all();
-    if($request->file('photo_id')){
-      return "photo exists";
+    if($file = $request->file('photo_id')){
+      $name = time() . $file->getClientOriginalName();
+      $file->move('images', $name);
+      $photo = Photo::create(['file' => $name]);
+      $input['photo_id'] = $photo->id;
     }
+    $input['password'] = bcrypt($request->password);
+    User::create($input);
     return redirect('/admin/users');
   }
 

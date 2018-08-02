c91fc02892f5b25ed0a45229d4ab97155b199c11
nauman-azeem <borissims4@gmail.com>
2018-08-03 02:30:34 +0500

    Update admin.posts.index view for categories & photo

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,9 +5,9 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>User</th>
         <th>Category</th>
-        <th>Photo</th>
         <th>Title</th>
         <th>Body</th>
         <th>Created</th>
@@ -19,9 +19,9 @@
         @foreach($posts as $post)
           <tr>
             <td>{{$post->id}}</td>
+            <td><img style="max-height: 50px" src="{{$post->photo?$post->photo->file: 'http://placehold.it/400x400'}}" alt="" /></td>
             <td>{{$post->user->name}}</td>
             <td>{{$post->category_id}}</td>
-            <td>{{$post->photo_id}}</td>
             <td>{{$post->title}}</td>
             <td>{{$post->body}}</td>
             <td>{{$post->created_at->diffForHumans()}}</td>

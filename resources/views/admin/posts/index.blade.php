d35a831a7f6d1f671630c12df62b6fc44007aaa5
nauman-azeem <borissims4@gmail.com>
2018-08-03 03:04:54 +0500

    Updated admin.posts.create with db categories

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -21,7 +21,7 @@
             <td>{{$post->id}}</td>
             <td><img style="max-height: 50px" src="{{$post->photo?$post->photo->file: 'http://placehold.it/400x400'}}" alt="" /></td>
             <td>{{$post->user->name}}</td>
-            <td>{{$post->category_id}}</td>
+            <td>{{$post->category ? $post->category->name : 'uncategorized'}}</td>
             <td>{{$post->title}}</td>
             <td>{{$post->body}}</td>
             <td>{{$post->created_at->diffForHumans()}}</td>

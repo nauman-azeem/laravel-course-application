e2be1ce855a47e04e6b52d1a570566c2ac511798
nauman-azeem <borissims4@gmail.com>
2018-08-03 15:17:14 +0500

    Set Up Admin Categories Functionallity

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Waldemiro
- * Date: 07/08/2016
- * Time: 12:44
- */
+@extends('layouts.admin')
+@section('content')
+  {!! Form::model($category, ['method'=>'PATCH', 'action'=>['AdminCategoriesController@update', $category->id]]) !!}
+    <div class="form-group">
+      {!! Form::label('name', 'Name:') !!}
+      {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+      {!! Form::submit('Update Category', ['class'=>'btn btn-primary']) !!}
+    </div>
+  {!! Form::close() !!}
+  {!! Form::open(['method'=>'DELETE', 'action'=>['AdminCategoriesController@destroy', $category->id]]) !!}
+    <div class="form-group">
+      {!! Form::submit('Delete Category', ['class'=>'btn btn-danger']) !!}
+    </div>
+  {!! Form::close() !!}
+@endsection
+@section('footer')
+@endsection
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Waldemiro
- * Date: 07/08/2016
- * Time: 12:42
- */
+@extends('layouts.admin')
+@section('content')
+  <h1>Categories</h1>
+  <div class="row">
+    <div class="col-sm-6">
+      {!! Form::open(['method'=>'POST', 'action'=>'AdminCategoriesController@store']) !!}
+        <div class="form-group">
+          {!! Form::label('name', 'Name:') !!}
+          {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+          {!! Form::submit('Create Category', ['class'=>'btn btn-primary']) !!}
+        </div>
+      {!! Form::close() !!}
+    </div>
+    <div class="col-sm-6">
+      @if($categories)
+        <table class="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Created</th>
+            </tr>
+          </thead>
+          <tbody>
+          @foreach($categories as $category)
+            <tr>
+              <td>{{$category->id}}</td>
+              <td><a href="{{route('admin.categories.edit', $category->id)}}">{{$category->name}}</a></td>
+              <td>{{$category->created_at ? $category->created_at->diffForHumans() : "no date"}}</td>
+            </tr>
+          @endforeach
+          </tbody>
+        </table>
+      @endif
+    </div>
+  </div>
+@endsection
+@section('footer')
+@endsection
